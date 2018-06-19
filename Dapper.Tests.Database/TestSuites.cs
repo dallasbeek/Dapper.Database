c88d3831010f3784ec8095dac85810491bbb950e
Dallas Beek <dallasbeek@gmail.com>
2018-06-19 16:43:05 -0500

    disable some suites

--- Dapper.Tests.Database/TestSuites.cs
+++ Dapper.Tests.Database/TestSuites.cs
@@ -91,64 +91,64 @@ namespace Dapper.Tests.Database
 #endif
     }
 
-    public class MySqlServerTestSuite : TestSuite
-    {
-        private const string DbName = "DapperContribTests";
+    //public class MySqlServerTestSuite : TestSuite
+    //{
+    //    private const string DbName = "DapperContribTests";
 
-        public static string ConnectionString { get; } =
-            IsAppVeyor
-                ? "Server=localhost;Uid=root;Pwd=Password12!;SslMode=none"
-                : "Server=localhost;Uid=test;Pwd=pass;";
+    //    public static string ConnectionString { get; } =
+    //        IsAppVeyor
+    //            ? "Server=localhost;Uid=root;Pwd=Password12!;SslMode=none"
+    //            : "Server=localhost;Uid=test;Pwd=pass;";
 
-        public override IDbConnection GetConnection()
-        {
-            if (_skip) throw new SkipTestException("Skipping MySQL Tests - no server.");
-            return new MySqlConnection(ConnectionString);
-        }
+    //    public override IDbConnection GetConnection()
+    //    {
+    //        if (_skip) throw new SkipTestException("Skipping MySQL Tests - no server.");
+    //        return new MySqlConnection(ConnectionString);
+    //    }
 
-        private static readonly bool _skip;
+    //    private static readonly bool _skip;
 
-        static MySqlServerTestSuite()
-        {
-            try
-            {
-                using (var connection = new MySqlConnection(ConnectionString))
-                {
-                    // ReSharper disable once AccessToDisposedClosure
-                    Action<string> dropTable = name => connection.Execute($"DROP TABLE IF EXISTS `{name}`;");
-                    connection.Open();
-                    connection.Execute($"DROP DATABASE IF EXISTS {DbName}; CREATE DATABASE {DbName}; USE {DbName};");
-                    dropTable("Stuff");
-                    connection.Execute("CREATE TABLE Stuff (TheId int not null AUTO_INCREMENT PRIMARY KEY, Name nvarchar(100) not null, Created DateTime null);");
-                    dropTable("People");
-                    connection.Execute("CREATE TABLE People (Id int not null AUTO_INCREMENT PRIMARY KEY, Name nvarchar(100) not null);");
-                    dropTable("Users");
-                    connection.Execute("CREATE TABLE Users (Id int not null AUTO_INCREMENT PRIMARY KEY, Name nvarchar(100) not null, Age int not null);");
-                    dropTable("Automobiles");
-                    connection.Execute("CREATE TABLE Automobiles (Id int not null AUTO_INCREMENT PRIMARY KEY, Name nvarchar(100) not null);");
-                    dropTable("Results");
-                    connection.Execute("CREATE TABLE Results (Id int not null AUTO_INCREMENT PRIMARY KEY, Name nvarchar(100) not null, `Order` int not null);");
-                    dropTable("ObjectX");
-                    connection.Execute("CREATE TABLE ObjectX (ObjectXId nvarchar(100) not null, Name nvarchar(100) not null);");
-                    dropTable("ObjectY");
-                    connection.Execute("CREATE TABLE ObjectY (ObjectYId int not null, Name nvarchar(100) not null);");
-                    dropTable("ObjectZ");
-                    connection.Execute("CREATE TABLE ObjectZ (Id int not null, Name nvarchar(100) not null);");
-                    dropTable("GenericType");
-                    connection.Execute("CREATE TABLE GenericType (Id nvarchar(100) not null, Name nvarchar(100) not null);");
-                    dropTable("NullableDates");
-                    connection.Execute("CREATE TABLE NullableDates (Id int not null AUTO_INCREMENT PRIMARY KEY, DateValue DateTime);");
-                }
-            }
-            catch (MySqlException e)
-            {
-                if (e.Message.Contains("Unable to connect"))
-                    _skip = true;
-                else
-                    throw;
-            }
-        }
-    }
+    //    static MySqlServerTestSuite()
+    //    {
+    //        try
+    //        {
+    //            using (var connection = new MySqlConnection(ConnectionString))
+    //            {
+    //                // ReSharper disable once AccessToDisposedClosure
+    //                Action<string> dropTable = name => connection.Execute($"DROP TABLE IF EXISTS `{name}`;");
+    //                connection.Open();
+    //                connection.Execute($"DROP DATABASE IF EXISTS {DbName}; CREATE DATABASE {DbName}; USE {DbName};");
+    //                dropTable("Stuff");
+    //                connection.Execute("CREATE TABLE Stuff (TheId int not null AUTO_INCREMENT PRIMARY KEY, Name nvarchar(100) not null, Created DateTime null);");
+    //                dropTable("People");
+    //                connection.Execute("CREATE TABLE People (Id int not null AUTO_INCREMENT PRIMARY KEY, Name nvarchar(100) not null);");
+    //                dropTable("Users");
+    //                connection.Execute("CREATE TABLE Users (Id int not null AUTO_INCREMENT PRIMARY KEY, Name nvarchar(100) not null, Age int not null);");
+    //                dropTable("Automobiles");
+    //                connection.Execute("CREATE TABLE Automobiles (Id int not null AUTO_INCREMENT PRIMARY KEY, Name nvarchar(100) not null);");
+    //                dropTable("Results");
+    //                connection.Execute("CREATE TABLE Results (Id int not null AUTO_INCREMENT PRIMARY KEY, Name nvarchar(100) not null, `Order` int not null);");
+    //                dropTable("ObjectX");
+    //                connection.Execute("CREATE TABLE ObjectX (ObjectXId nvarchar(100) not null, Name nvarchar(100) not null);");
+    //                dropTable("ObjectY");
+    //                connection.Execute("CREATE TABLE ObjectY (ObjectYId int not null, Name nvarchar(100) not null);");
+    //                dropTable("ObjectZ");
+    //                connection.Execute("CREATE TABLE ObjectZ (Id int not null, Name nvarchar(100) not null);");
+    //                dropTable("GenericType");
+    //                connection.Execute("CREATE TABLE GenericType (Id nvarchar(100) not null, Name nvarchar(100) not null);");
+    //                dropTable("NullableDates");
+    //                connection.Execute("CREATE TABLE NullableDates (Id int not null AUTO_INCREMENT PRIMARY KEY, DateValue DateTime);");
+    //            }
+    //        }
+    //        catch (MySqlException e)
+    //        {
+    //            if (e.Message.Contains("Unable to connect"))
+    //                _skip = true;
+    //            else
+    //                throw;
+    //        }
+    //    }
+    //}
 
     public class SQLiteTestSuite : TestSuite
     {
@@ -180,37 +180,37 @@ namespace Dapper.Tests.Database
         }
     }
 
-#if NET452
-    public class SqlCETestSuite : TestSuite
-    {
-        const string FileName = "Test.DB.sdf";
-        public static string ConnectionString => $"Data Source={FileName};";
-        public override IDbConnection GetConnection() => new SqlCeConnection(ConnectionString);
+//#if NET452
+//    public class SqlCETestSuite : TestSuite
+//    {
+//        const string FileName = "Test.DB.sdf";
+//        public static string ConnectionString => $"Data Source={FileName};";
+//        public override IDbConnection GetConnection() => new SqlCeConnection(ConnectionString);
 
-        static SqlCETestSuite()
-        {
-            if (File.Exists(FileName))
-            {
-                File.Delete(FileName);
-            }
-            var engine = new SqlCeEngine(ConnectionString);
-            engine.CreateDatabase();
-            using (var connection = new SqlCeConnection(ConnectionString))
-            {
-                connection.Open();
-                connection.Execute(@"CREATE TABLE Stuff (TheId int IDENTITY(1,1) not null, Name nvarchar(100) not null, Created DateTime null) ");
-                connection.Execute(@"CREATE TABLE People (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null) ");
-                connection.Execute(@"CREATE TABLE Users (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null, Age int not null) ");
-                connection.Execute(@"CREATE TABLE Automobiles (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null) ");
-                connection.Execute(@"CREATE TABLE Results (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null, [Order] int not null) ");
-                connection.Execute(@"CREATE TABLE ObjectX (ObjectXId nvarchar(100) not null, Name nvarchar(100) not null) ");
-                connection.Execute(@"CREATE TABLE ObjectY (ObjectYId int not null, Name nvarchar(100) not null) ");
-                connection.Execute(@"CREATE TABLE ObjectZ (Id int not null, Name nvarchar(100) not null) ");
-                connection.Execute(@"CREATE TABLE GenericType (Id nvarchar(100) not null, Name nvarchar(100) not null) ");
-                connection.Execute(@"CREATE TABLE NullableDates (Id int IDENTITY(1,1) not null, DateValue DateTime null) ");
-            }
-            Console.WriteLine("Created database");
-        }
-    }
-#endif
+//        static SqlCETestSuite()
+//        {
+//            if (File.Exists(FileName))
+//            {
+//                File.Delete(FileName);
+//            }
+//            var engine = new SqlCeEngine(ConnectionString);
+//            engine.CreateDatabase();
+//            using (var connection = new SqlCeConnection(ConnectionString))
+//            {
+//                connection.Open();
+//                connection.Execute(@"CREATE TABLE Stuff (TheId int IDENTITY(1,1) not null, Name nvarchar(100) not null, Created DateTime null) ");
+//                connection.Execute(@"CREATE TABLE People (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null) ");
+//                connection.Execute(@"CREATE TABLE Users (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null, Age int not null) ");
+//                connection.Execute(@"CREATE TABLE Automobiles (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null) ");
+//                connection.Execute(@"CREATE TABLE Results (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null, [Order] int not null) ");
+//                connection.Execute(@"CREATE TABLE ObjectX (ObjectXId nvarchar(100) not null, Name nvarchar(100) not null) ");
+//                connection.Execute(@"CREATE TABLE ObjectY (ObjectYId int not null, Name nvarchar(100) not null) ");
+//                connection.Execute(@"CREATE TABLE ObjectZ (Id int not null, Name nvarchar(100) not null) ");
+//                connection.Execute(@"CREATE TABLE GenericType (Id nvarchar(100) not null, Name nvarchar(100) not null) ");
+//                connection.Execute(@"CREATE TABLE NullableDates (Id int IDENTITY(1,1) not null, DateValue DateTime null) ");
+//            }
+//            Console.WriteLine("Created database");
+//        }
+//    }
+//#endif
 }
