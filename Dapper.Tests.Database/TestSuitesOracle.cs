64c17604b2a67d19355fbc1998326c730aa2d138
Chris Donnelly <chris.donnelly@coxautoinc.com>
2018-08-31 16:48:33 -0500

    Argh.  Forgot to revert to the original connection string.

--- Dapper.Tests.Database/TestSuitesOracle.cs
+++ Dapper.Tests.Database/TestSuitesOracle.cs
@@ -15,8 +15,8 @@ namespace Dapper.Tests.Database
     [Trait("Provider", "Oracle")]
     public partial class OracleTestSuite : TestSuite
     {
-        //public static string ConnectionString => $"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=Denver)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)));User Id=testuser;Password=Password12!;";
-﻿        public static string ConnectionString => "User Id=testuser;Password=Password12!;Data Source=localhost:1521/XE.localdomain";
+        public static string ConnectionString => $"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=Denver)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)));User Id=testuser;Password=Password12!;";
+        //public static string ConnectionString => "User Id=testuser;Password=Password12!;Data Source=localhost:1521/XE.localdomain";
 
         protected override string P => ":";
 
