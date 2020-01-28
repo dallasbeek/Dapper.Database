a8e8529dc3e052754979973afebfaddc1dc7cf7a
Chris Donnelly <chris.donnelly@coxautoinc.com>
2020-01-28 16:00:53 -0600

    We can now run PostgreSQL tests on a fresh database.
    I finally figured out how to fall back and create the test database.

--- Dapper.Tests.Database/TestSuitesPostgres.cs
+++ Dapper.Tests.Database/TestSuitesPostgres.cs
@@ -41,19 +41,47 @@ namespace Dapper.Tests.Database
             SqlMapper.AddTypeHandler<Guid>(new GuidTypeHandler());
             try
             {
-                using (var connection = new NpgsqlConnection(ConnectionString))
-                {
-                    connection.Open();
-
-                    var awfile = File.ReadAllText(".\\Scripts\\postgresawlite.sql");
-                    connection.Execute(awfile);
-                    connection.Execute("delete from Person;");
-
-                }
+                PopulateDatabase();
+            }
+            catch (PostgresException e) when (e.Message.Contains($"database \"{DbName}\" does not exist"))
+            {
+                // PostgreSQL doesn't have a good way to detect if the "Test" database already exists:
+                //  - Your connection string has to include the database you want
+                //  - Their version of CREATE DATABASE does not have IF NOT EXISTS support
+                CreateTestDatabase();
+                PopulateDatabase();
             }
             catch (SocketException e) when (e.Message.Contains("No connection could be made because the target machine actively refused it"))
             {
                 _skip = true;
+            }
+        }
+
+        /// <summary>
+        /// Connects to the default database and creates the test database.
+        /// </summary>
+        private static void CreateTestDatabase()
+        {
+            var cs = new NpgsqlConnectionStringBuilder(ConnectionString);
+            var database = cs.Database;
+            cs.Database = null;
+            using (var connection = new NpgsqlConnection(cs.ToString()))
+            {
+                connection.Open();
+                connection.Execute($"create database \"{database}\"");
+            }
+
+        }
+
+        private static void PopulateDatabase()
+        {
+            using (var connection = new NpgsqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var awfile = File.ReadAllText(".\\Scripts\\postgresawlite.sql");
+                connection.Execute(awfile);
+                connection.Execute("delete from Person;");
             }
         }
     }
